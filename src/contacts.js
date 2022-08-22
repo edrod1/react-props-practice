f0d78433b93fe6dfbe321a7c93303d963950a0b0
Edgar <edgarx28@outlook.com>
2022-08-22 14:12:43 -0400

    Initial commit

---
+++ src/contacts.js
@@ -0,0 +1,22 @@
+const contacts = [
+  {
+    name: "Ryan Reynolds",
+    imgURL: "https://bit.ly/3wkAfAl",
+    phone: "+123 456 789",
+    email: "R@Reynolds.com"
+  },
+  {
+    name: "Katt Williams",
+    imgURL: "https://s3.amazonaws.com/oratv-admin-wysiwyg/2017/12/13/96f7023fa185a99b69161d3a7904d118.jpg",
+    phone: "202-555-0163",
+    email: "Katt@Katty.com"
+  },
+  {
+    name: "Elon Musk",
+    imgURL:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBtrXm5uD9r1hTolGGhnsN7NeBU8Zxs_zfgA&usqp=CAU",
+    phone: "+321 123 212",
+    email: "Tesla@Elonm.com"
+  }
+];
+export default contacts;
